721345023c2519efa49f8cf351fa0600ccab6110
02xa <bcgoochyfreitas2203@gmail.com>
2026-01-11 01:46:02 -0300

    feat(nvim): Alot of new changes.

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,16 +1,13 @@
--- Delete this condition if you want to execute the file
-if true then
-  return {}
-end
-
--- Example of disabling some plugins. Add yours
 local disabled = {
-  {
-    "akinsho/bufferline.nvim",
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-  },
+  { "akinsho/bufferline.nvim" },
+  { "nvim-neo-tree/neo-tree.nvim" },
+  { "folke/flash.nvim" },
+  { "folke/todo-comments.nvim" },
+  { "folke/noice.nvim" },
+  { "folke/trouble.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "MunifTanjim/nui.nvim" },
+  { "linux-cultist/venv-selector.nvim" },
 }
 
 for i, plugin in ipairs(disabled) do
--- .config/nvim/lua/plugins/extras/dap/core-extended.lua
+++ .config/nvim/lua/plugins/extras/dap/core-extended.lua
@@ -1,33 +1,48 @@
+local prefix = "<leader>d"
+
 return {
   { import = "lazyvim.plugins.extras.dap.core" },
   {
     "mfussenegger/nvim-dap",
-    opts = {
-      defaults = {
-        fallback = {
-          external_terminal = {
-            command = "/usr/bin/kitty",
-            args = { "--class", "kitty-dap", "--hold", "--detach", "nvim-dap", "-c", "DAP" },
+    dependencies = {
+      "igorlfs/nvim-dap-view",
+      opts = {
+        winbar = {
+          sections = { "watches", "scopes", "exceptions", "breakpoints", "threads", "repl", "sessions", "console" },
+          default_section = "scopes",
+        },
+        windows = {
+          terminal = {
+            size = 0.2,
+            position = "left",
+            hide = {},
+            start_hidden = false,
           },
         },
+        auto_toggle = true,
+      },
+      -- stylua: ignore
+      keys = {
+        { prefix .. "u", "<cmd>DapViewToggle<cr>", desc = "Dap UI" },
+        { prefix .. "a", function() require("dap-view").add_expr() end, desc = "Add Expression" },
       },
     },
     -- stylua: ignore
     keys = {
-      { "<F5>", function() require("dap").continue() end, desc = "Debug: Continue" },
-      { "<F10>", function() require("dap").step_over() end, desc = "Debug: Step over" },
-      { "<F11>", function() require("dap").step_into() end, desc = "Debug: Step into" },
-      { "<F12>", function() require("dap").step_out() end, desc = "Debug: Step out" },
-      { "<F2>", function() require("dap").toggle_breakpoint() end, desc = "Debug: Toggle breakpoint" },
-      { "<S-F2>", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+      { prefix .. "O", function() require("dap").step_out() end, desc = "Debug: Step out" },
+      { prefix .. "o", function() require("dap").step_over() end, desc = "Debug: Step over" },
     },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    enabled = false,
   },
   {
     "nvim-neotest/neotest",
     optional = true,
     -- stylua: ignore
     keys = {
-      { "<leader>tL", function() require("neotest").run.run_last({ strategy = "dap" }) end, desc = "Debug Last Test" },
+      { "<leader>tD", function() require("neotest").run.run_last({ strategy = "dap" }) end, desc = "Debug Last" },
     },
   },
 }
